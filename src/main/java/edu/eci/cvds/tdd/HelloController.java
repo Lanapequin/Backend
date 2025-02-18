7eea768ec4b5d06aefbabf9dbfeaffb3ed7bac87
unknown <santiago.amaya-z@labinfo.is.escuelaing.edu.co>
2025-02-18 10:34:38 -0500

    Integración SpringBoot

---
+++ src/main/java/edu/eci/cvds/tdd/HelloController.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.tdd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "¡Hola desde Spring Boot!";
+    }
+}
